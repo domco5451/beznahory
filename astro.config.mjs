b74720fed20186ba541d89c6cc8c1ee431980471
domco5451 <domco.ko@gmail.com>
2025-08-05 14:53:58 +0200

    add new stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
+import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), icon({
-    include: {
-      mdi: ["*"],
-    }
-  })],
+  integrations: [
+    react(),
+    mdx(),
+    icon({
+      include: {
+        mdi: ["*"],
+      },
+    }),
+    compress({
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+    }),
+  ],
 
   markdown: {
     remarkPlugins: [remarkReadingTime],
