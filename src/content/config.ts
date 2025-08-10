e2e299c52a3ff9b1b885de65402f83299ceac794
domco5451 <domco.ko@gmail.com>
2025-08-10 20:16:35 +0200

    commit before merge

---
+++ src/content/config.ts
@@ -0,0 +1,32 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            pubDate: z.coerce.date(),
+            description: z.string().optional(),
+            author: z.string(),
+            kraj: z.string().optional(),
+            okres: z.string().optional(),
+            thumbnail: image(),
+            thumbnailAlt: z.string(),
+            pfp: image(),
+            pfpAlt: z.string(),
+            tags: z.array(z.string()).optional(),
+            latitude: z.number().optional(),
+            longitude: z.number().optional(),
+            gpx: z.string().optional(),
+            ratings: z.object({
+                difficulty: z.number().optional(),
+                views: z.number().optional(),
+                length: z.string().optional(),
+                time: z.string().optional(),
+                terrain: z.string().optional(),
+            }).optional(),
+            gallery: z.array(image()).optional(),
+            galleryAlts: z.array(z.string()).optional(),
+        }),    
+})
+
+export const collections = { blog: blogCollection, };
