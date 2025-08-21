c7f5fe8a8e575966c832c84d06af03408b37d3c4
domco5451 <domco.ko@gmail.com>
2025-08-21 21:30:06 +0200

    update badge interactions

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface Window {
-  theme: {
-    setTheme: (theme: "auto" | "dark" | "light") => void;
-    getTheme: () => "auto" | "dark" | "light";
-    getSystemTheme: () => "light" | "dark";
-    getDefaultTheme: () => "auto" | "dark" | "light";
-  };
-}
