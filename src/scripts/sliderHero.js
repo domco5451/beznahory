6777f86802ca5f7faa0c99b7399679356ffea50e
domco5451 <domco.ko@gmail.com>
2025-08-22 23:01:26 +0200

    add grabCursor

--- src/scripts/sliderHero.js
+++ src/scripts/sliderHero.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
+    grabCursor: true,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
